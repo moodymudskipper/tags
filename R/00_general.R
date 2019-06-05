c12a6b15c2e73bdb197131c13e62aba5559d5680
moodymudskipper <antoine.fabri@gmail.com>
2019-06-05 21:57:15 +0200

    import tag::tag for easier definitions as we don't depend on tag anymore

--- R/00_general.R
+++ R/00_general.R
@@ -1,15 +1,19 @@
-rox <- function(f){
-  cat("#'", substitute(f), "\n#'\n")
-  cat(paste("#' @param", formalArgs(f), collapse="\n"))
-  cat("\n#' @export\n#' @examples\n")
-}
+# rox <- function(f){
+#   cat("#'", substitute(f), "\n#'\n")
+#   cat(paste("#' @param", formalArgs(f), collapse="\n"))
+#   cat("\n#' @export\n#' @examples\n")
+# }
 
 # imports
 # @import tags tag
 NULL
 
 # tag <- getFromNamespace("tag","tags")
+
+tag <- getFromNamespace("tag","tag")
 add_class <- getFromNamespace("add_class","tag")
 tbl_at_syms <- getFromNamespace("tbl_at_syms", "dplyr")
 capture_error <- getFromNamespace("capture_error","purrr")
-capture_output <- getFromNamespace("capture_error","purrr")
+capture_output <- getFromNamespace("capture_output","purrr")
+
+
